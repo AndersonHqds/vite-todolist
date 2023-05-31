bba552a7c18a1914bc5f3223ceca0ecdd65fb22a
LorranLopes <lorranlop42@gmail.com>
2023-05-30 22:18:11 -0300

    Create the Sign Up page

---
+++ src/pages/todo/Todo.tsx
@@ -0,0 +1,18 @@
+import {
+    Container,
+    Header
+
+}from "./style";
+import logo from "../../assets/logo.png"
+function Todo(){
+  return(
+    
+    <Container>
+    <Header/>
+    </Container>
+  )
+
+
+
+}
+export default Todo
