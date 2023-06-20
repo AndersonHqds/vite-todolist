9fb2ed98f181cce3f5c2ecf790c10f28e48e8998
LorranLopes <lorranlop42@gmail.com>
2023-06-20 19:56:41 -0300

    create the signup feature

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -8,7 +8,21 @@ import {
     InputContainer
 } from "./style";
 import logo from "../../assets/logo.png"
+import { useState } from "react"
+import axios from "axios"
 function SignUp() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const onSubmit = (event: any) => {
+        event.preventDefault()
+        axios.post("http://localhost:3001/signup", {
+            username: username,
+            password: password
+        })
+    }
+
     return (
         <Container>
             <Form>
@@ -16,11 +30,13 @@ function SignUp() {
                     <LogoImg src={logo} />
                 </LogoContainer>
                 <InputContainer>
-                    <Input placeholder="Username" />
+                    <Input placeholder="Username" onChange={event => setUsername(event.target.value)} />
 
-                    <Input placeholder="Password" type="password" />
-                    <Input placeholder="Confirm your password" type="password" />
-                    <Button>
+                    <Input placeholder="Password" type="password" onChange={event => setPassword(event.target.value)} />
+                    <Input placeholder="Confirm your password" type="password" onChange={event => setConfirmPassword(event.target.value)} />
+                    <Button disabled={username.length === 0 || password.length === 0 || confirmPassword.length === 0}
+                    onClick={onSubmit}>
+
                         Sign Up
                     </Button>
                 </InputContainer>
